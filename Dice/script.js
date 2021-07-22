bdcfeaa8e91a7265a5a718a8ebe92ce4cae7f1aa
saturnhafen <saturnhafen@gmail.com>
2021-07-22 13:22:57 +0200

    fix code style

--- Dice/script.js
+++ Dice/script.js
@@ -1,10 +1,10 @@
 // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+	return Math.floor(Math.random() * max);
 }
 
 function roll() {
-	const diceCount = document.getElementById('count').value;
+	const diceCount = Number(document.getElementById("count").value); // remember to cast to number! :)
 	
 	let values = [];
 	
@@ -13,18 +13,26 @@ function roll() {
 		values.push(getRandomInt(6) + 1 /* 1 - max, not 0 - (max - 1)*/);
 	}
 	
-	const resultContainer = document.getElementById('result-visual');
+	const resultContainer = document.getElementById("result-visual");
 	
 	// remove previous dice faces
 	while (resultContainer.firstChild) {
-  	resultContainer.removeChild(resultContainer.firstChild);
-  }
+		resultContainer.removeChild(resultContainer.firstChild);
+	}
 	
 	// add current dice faces
-	values.forEach((x, i) => {
-		let dice = document.getElementById('dice-' + x);
+	for (let i = 0; i < diceCount; i++) {
+		console.log(values);
+		let dice = document.getElementById("dice-" + values[i]);
 		resultContainer.appendChild(dice.cloneNode(true));
-	});
+	}
 	
-	document.getElementById('result').value = values.join(' + ');
+	document.getElementById("result").value = values.join(" + ");
+	
+	let sum = 0;
+	for (let i = 0; i < diceCount; i++) {
+		sum += values[i];
+	}
+	
+	document.getElementById("total").value = sum;
 }
